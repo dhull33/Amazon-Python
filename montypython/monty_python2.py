b36d854cf9e07ea3d42565567dae3397163998b9
David Hull <david.hull37@gmail.com>
2020-06-29 18:20:47 -0400

    First commit

---
+++ montypython/monty_python2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def main():
+    
+    round = 0
+
+    while True:
+        round = round + 1
+    
+        print('Finish the movie, title, "Monty Python\'s The Life of _____"')
+
+        answer = input('Your guess--> ')
+
+        if (answer == 'Brian'):
+            print('Correct!')
+            break
+    
+        elif round == 3:
+            print('Sorry, the answer was Brian.')
+            break
+
+        else:
+            print(answer)
+            print('Sorry! Try again!')
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ mylist01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def main():
+    movies = []
+    movies.append("Avatar")
+    movies.append("Back to the Future")
+
+    print(movies)
+
+
+# Zach says this is the best way to run the main function if we are running the script
+# manually?
+if __name__== "__main__":
+    main()
+
+
+
+
