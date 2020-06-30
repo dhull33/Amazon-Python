27a3c5dae07c9f786e7e301cf32350fcc4ce4d6a
David Hull <david.hull37@gmail.com>
2020-06-30 17:45:36 -0500

    Added two more looping excersises

---
+++ fact/looping02b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    # open file in read mode
+    with open("dnsservers.txt", "r") as dnsfile:
+        dnslist = dnsfile.readlines()
+        
+        for server in dnslist:
+            print(server, end = "")
+            # No need to close file because with close it automatically
+
+if __name__ == "__main__":
+    main()
